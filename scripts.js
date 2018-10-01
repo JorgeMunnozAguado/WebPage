f020394b41ec55feb95db2df30b8cc71d50d0173
Jorge Muñoz Aguado <jorge.munnoza@estudiante.uam.es>
2018-10-01 23:50:40 +0200

    Reducing size of images.

--- scripts.js
+++ scripts.js
@@ -1,21 +1,33 @@
-
+var header;
+var sticky;
 
 function moveLetter( name, size ) { 
     document.getElementById( name ).style.height = size;
 }
     
-function prueba() {
+function scrolling() {
     
-    var marginTop = -(document.documentElement.scrollTop) * 0.6;
-    
-    document.getElementById('body-info').style.marginTop = marginTop + 'px';
+    if (window.pageYOffset > sticky) {
+        
+        header.classList.add('sticky');
+            
+    } else {
+        
+        header.classList.remove('sticky');
+        
+        var marginTop = -(document.documentElement.scrollTop) * 0.6;
+        document.getElementById('body-info').style.marginTop = marginTop + 'px';
+
+    }
 }
 
 function setPaddingImage( ) {
     
     var pageHeight = screen.height;
-    var pageWidth = screen.width;
 
     document.getElementById('header-div').style.height = pageHeight + 'px';
     document.getElementById('header-letters').style.marginTop = (pageHeight * 2 / 6) + 'px';
+    
+    header = document.getElementById('header-name');
+    sticky = header.offsetTop;
 }
